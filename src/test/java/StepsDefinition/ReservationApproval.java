601f6903147077576044dc2c5563b97b7d3deb26
nasser <nasseraker4@gmail.com>
2024-03-08 01:10:18 +0200

    nasser commit

---
+++ src/test/java/StepsDefinition/ReservationApproval.java
@@ -0,0 +1,48 @@
+package StepsDefinition;
+
+import io.cucumber.java.en.*;
+
+public class ReservationApproval {
+
+    @Given("there are pending reservation requests")
+    public void there_are_pending_reservation_requests() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I select a pending reservation request")
+    public void i_select_a_pending_reservation_request() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I click on the {string} button")
+    public void i_click_on_the_button(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the reservation should be marked as approved")
+    public void the_reservation_should_be_marked_as_approved() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the requester should receive a confirmation message")
+    public void the_requester_should_receive_a_confirmation_message() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the reservation should be marked as denied")
+    public void the_reservation_should_be_marked_as_denied() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the requester should receive a notification with the denial and the reason")
+    public void the_requester_should_receive_a_notification_with_the_denial_and_the_reason() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
