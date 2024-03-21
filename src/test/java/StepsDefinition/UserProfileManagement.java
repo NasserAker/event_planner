90c4e744547cde9ef85a1e0c21e14eecd90ad439
Talah Qamhieh <s12027674@stu.najah.edu>
2024-03-21 17:23:59 +0200

    TALAH

---
+++ src/test/java/StepsDefinition/UserProfileManagement.java
@@ -0,0 +1,75 @@
+package StepsDefinition;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class UserProfileManagement {
+
+    @Given("the user is logged in")
+    public void the_user_is_logged_in() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("they navigate to the profile page")
+    public void they_navigate_to_the_profile_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("they should see their profile details")
+    public void they_should_see_their_profile_details() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("they update their profile information")
+    public void they_update_their_profile_information() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("they submit the changes")
+    public void they_submit_the_changes() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("they should see a success message")
+    public void they_should_see_a_success_message() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("their profile should be updated accordingly")
+    public void their_profile_should_be_updated_accordingly() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("they navigate to the change password page")
+    public void they_navigate_to_the_change_password_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("they enter their current password and new password")
+    public void they_enter_their_current_password_and_new_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("they submit the change password form")
+    public void they_submit_the_change_password_form() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("they should be able to login with the new password")
+    public void they_should_be_able_to_login_with_the_new_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
