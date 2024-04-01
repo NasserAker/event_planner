acd6335fb56d9e27a4c4f51e10dd820e1c514988
Talah Qamhieh <s12027674@stu.najah.edu>
2024-04-01 16:21:43 +0300

    TALAH

---
+++ src/test/java/StepsDefinition/viewuserstep.java
@@ -0,0 +1,21 @@
+package StepsDefinition;
+
+public class viewuserstep {
+    @Given("the user is logged in")
+    public void the_user_is_logged_in() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("they choose to view their profile")
+    public void they_choose_to_view_their_profile() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("they should see their profile information")
+    public void they_should_see_their_profile_information() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
