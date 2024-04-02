3ab9219bac1724e2e4c961ea813d080a602920cc
Raya O. Break <s12028604@stu.najah.edu>
2024-04-02 15:09:56 +0300

    .

--- src/main/java/ApplicationClasses/ReservationRequest.java
+++ src/main/java/ApplicationClasses/ReservationRequest.java
@@ -1,5 +1,49 @@
 package ApplicationClasses;
 
-public class ReservationRequest {
+import java.util.List;
 
+public class ReservationRequest {
+    private Venue selectedVenue;
+    private Date selectedDate;
+    private List<AdditionalService> selectedServices;
+    private User user;
+
+    public ReservationRequest(Venue selectedVenue, Date selectedDate, List<AdditionalService> selectedServices, User user) {
+        this.selectedVenue = selectedVenue;
+        this.selectedDate = selectedDate;
+        this.selectedServices = selectedServices;
+        this.user = user;
+    }
+
+    public Venue getSelectedVenue() {
+        return selectedVenue;
+    }
+
+    public void setSelectedVenue(Venue selectedVenue) {
+        this.selectedVenue = selectedVenue;
+    }
+
+    public Date getSelectedDate() {
+        return selectedDate;
+    }
+
+    public void setSelectedDate(Date selectedDate) {
+        this.selectedDate = selectedDate;
+    }
+
+    public List<AdditionalService> getSelectedServices() {
+        return selectedServices;
+    }
+
+    public void setSelectedServices(List<AdditionalService> selectedServices) {
+        this.selectedServices = selectedServices;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
