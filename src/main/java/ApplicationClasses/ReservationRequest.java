430e3a767beae8ab27afd8ed9c59e2a639170c3f
hala <s12112519@stu.najah.edu>
2024-04-05 16:34:27 +0300

    hi

--- src/main/java/ApplicationClasses/ReservationRequest.java
+++ src/main/java/ApplicationClasses/ReservationRequest.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 import java.util.List;
+import ApplicationClasses.Date; // Import your custom Date class
 
 public class ReservationRequest {
     private int requestId;
--- src/main/java/ApplicationClasses/Venue.java
+++ src/main/java/ApplicationClasses/Venue.java
@@ -1,6 +1,7 @@
 package ApplicationClasses;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Venue {
