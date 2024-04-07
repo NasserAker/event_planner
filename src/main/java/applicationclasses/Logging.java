3fdeb244c98a40e5e115a16624a87a45c07158a2
Raya O. Break <s12028604@stu.najah.edu>
2024-04-07 09:36:05 +0300

    raya commit

--- src/main/java/applicationclasses/Logging.java
+++ src/main/java/applicationclasses/Logging.java
@@ -9,8 +9,23 @@ import static applicationclasses.User.allUsers;
 
 public class Logging {
 
+    private String emailToCheck;
+    private boolean successfulpassword= false;
+
+    private String email;
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+
+
     private String password;
     private boolean logState = false;
+
+
+
 
     private static int y;
     int type = -3;
@@ -20,19 +35,21 @@ public class Logging {
 
 
     public int searchEmail(String email) {
-        String emailToCheck = email;
 
+        emailToCheck = email;
         for (Admin admin : adminList) {
             if (email.equals(admin.getEmail())) {
                 return 0;
             }
         }
 
+
         for (User user : allUsers) {
             if (email.equals(user.getEmail())) {
                 return 1;
             }
         }
+
 
         for (ServiceProvider serviceProvider : ServiceProvider.getServiceProviderList()) {
             if (email.equals(serviceProvider.getEmail())) {
@@ -43,59 +60,90 @@ public class Logging {
         return -1;
     }
 
-    public int searchPassword(String email, String password) {
-        String value = q.get(email);
 
-        boolean successfulpassword = password.equals(value);
 
-        if (successfulpassword) {
+
+
+
+
+
+
+    public int searchPassword(String password) {
+
+        String value = q.get(emailToCheck);
+
+        if (password.equals(value)) {
+            successfulpassword = true;
             logState = true;
             return y;
         } else {
+            successfulpassword = false;
             return -33;
         }
     }
 
+
+
+
+
+
     public static Map<String,String> getQ(){
         return q;
-    }
 
+    }
     public void logState(boolean t) {
+
         logState=t;
     }
 
-    public boolean login(String email, String password) {
+    public boolean login(String password) {
+
         if(this.password.equals(password)) {
             logger.info("successfully logged in");
             logState=true;
             return true;
-        } else {
+
+        }
+        else {
             logger.info("wrong password");
             return false;
         }
     }
 
     public void setLogState(boolean t) {
+
         logState=t;
     }
-
     public boolean getLogState() {
         return logState;
     }
-
     public void logout() {
+
         logState = false;
     }
 
     public int searchUser(String email, String password) {
+
         int userType = searchEmail(email);
 
         if (userType >= 0) {
-            int passwordCheck = searchPassword(email, password);
+
+            int passwordCheck = searchPassword(password);
             if (passwordCheck == -33) {
+
                 return userType;
             }
         }
+
+
         return -1;
     }
 }
+
+
+
+
+
+
+
+
