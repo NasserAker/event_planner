622307d766c0837ee2eb29dfdebfc6ab07e2fc7c
Raya O. Break <s12028604@stu.najah.edu>
2024-04-07 23:43:42 +0300

    raya commit

---
+++ src/test/java/StepsDefinition/adduserstep.java
@@ -0,0 +1,17 @@
+package StepsDefinition;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import applicationclasses.User;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class adduserstep {
+
+
+
+    }
+
